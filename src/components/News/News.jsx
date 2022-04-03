17373a9d1a5080155ac5028b3bf9e516650c6fcd
Towfik <tufikhasan05@gmail.com>
2022-04-03 16:18:18 +0600

    news item component design completed

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,17 +1,28 @@
-import React, { Component } from 'react'
-import './News.scss';
-import Newsitem from '../Newsitem/Newsitem'
+import React, { Component } from "react";
+import "./News.scss";
+import Newsitem from "../Newsitem/Newsitem";
 
 export default class News extends Component {
-
   render() {
     return (
-      <>
-        News
-        <Newsitem />
-        <Newsitem />
-        <Newsitem />
-      </>
-    )
+      <div className="app__news">
+        <Newsitem title="News one" description="description" source="source" />
+        <Newsitem
+          title="News one2"
+          description="description2"
+          source="source2"
+        />
+        <Newsitem
+          title="News one3"
+          description="description3"
+          source="source3"
+        />
+        <Newsitem
+          title="News one4"
+          description="description4"
+          source="source4"
+        />
+      </div>
+    );
   }
 }
--- src/components/Newsitem/Newsitem.jsx
+++ src/components/Newsitem/Newsitem.jsx
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
-import './Newsitem.scss';
+import React, { Component } from "react";
+import "./Newsitem.scss";
 
 export default class Newsitem extends Component {
-
   render() {
+    let { imageUrl, title, description, source, newsUrl, author, date } =
+      this.props;
     return (
-      <div>Newsitem ..</div>
-    )
+      <div className="app__news-item app__flex">
+        <div className="app__news-img app__flex">
+          <img
+            src={
+              !imageUrl
+                ? "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/223LNQCYDYI6ZGQYUUDM6OVDDU.jpg&w=1440"
+                : imageUrl
+            }
+            alt={title}
+          />
+        </div>
+        <div className="app__news-content">
+          <h4 className="bold-text">{title}</h4>
+          <p class="p-text">
+            <small class="text-muted">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toLocaleString()}
+            </small>
+          </p>
+          <p className="p-text des" style={{ marginTop: 5 }}>
+            {description}
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="app__btns_primary"
+          >
+            Read more
+          </a>
+          <div className="app__news-tag app__flex">
+            <p className="p-text">{source}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }

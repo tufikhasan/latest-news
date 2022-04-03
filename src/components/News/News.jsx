0473f9e65b52c2788a9063378c83174bebe461bc
Towfik <tufikhasan05@gmail.com>
2022-04-03 16:46:16 +0600

    news heading  added

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -6,22 +6,31 @@ export default class News extends Component {
   render() {
     return (
       <div className="app__news">
-        <Newsitem title="News one" description="description" source="source" />
-        <Newsitem
-          title="News one2"
-          description="description2"
-          source="source2"
-        />
-        <Newsitem
-          title="News one3"
-          description="description3"
-          source="source3"
-        />
-        <Newsitem
-          title="News one4"
-          description="description4"
-          source="source4"
-        />
+        <h2 className="head-text">
+          Latest News - <span>Top Heading</span>
+        </h2>
+        <div className="app__wrapper">
+          <Newsitem
+            title="News one"
+            description="description"
+            source="source"
+          />
+          <Newsitem
+            title="News one2"
+            description="description2"
+            source="source2"
+          />
+          <Newsitem
+            title="News one3"
+            description="description3"
+            source="source3"
+          />
+          <Newsitem
+            title="News one4"
+            description="description4"
+            source="source4"
+          />
+        </div>
       </div>
     );
   }

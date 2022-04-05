a43d2331727ade6dbacaaf9e7ab05a38d9c8c8ce
Towfik <tufikhasan05@gmail.com>
2022-04-05 14:46:29 +0600

    Pagination added

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,46 +1,103 @@
 import React, { Component } from "react";
 import "./News.scss";
 import Newsitem from "../Newsitem/Newsitem";
+import PropTypes from "prop-types";
 
 export default class News extends Component {
-  constructor() {
-    super();
+  static defaultProps = {
+    pageSize: 8,
+    country: "us",
+  };
+  static propTypes = {
+    pageSize: PropTypes.number,
+    country: PropTypes.string,
+  };
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
+      page: 1,
+      totalResults: "",
     };
   }
-  async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=824fba3880914e30accf0303f3427890";
+  async updateNews() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=824fba3880914e30accf0303f3427890&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let reecivedData = await data.json();
     this.setState({
       articles: reecivedData.articles,
+      totalResults: reecivedData.totalResults,
     });
   }
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  handlePrevBtn = async () => {
+    this.updateNews();
+    this.setState({
+      page: this.state.page - 1,
+    });
+  };
+  handleNextBtn = async () => {
+    this.updateNews();
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      this.setState({
+        page: this.state.page + 1,
+      });
+    }
+  };
   render() {
     return (
-      <div className="app__news">
-        <h2 className="head-text">
-          Latest News - <span>Top Heading</span>
-        </h2>
-        <div className="app__wrapper">
-          {this.state.articles.map((news, index) => {
-            return (
-              <Newsitem
-                key={index}
-                imageUrl={news.urlToImage}
-                title={`${news.title.slice(0, 55)}..`}
-                description={news.description.slice(0, 88)}
-                source={news.source.name}
-                newsUrl={news.url}
-                author={news.author}
-                date={news.publishedAt}
-              />
-            );
-          })}
+      <>
+        <div className="app__news">
+          <h2 className="head-text">
+            Latest News - <span>Top Heading</span>
+          </h2>
+          <div className="app__wrapper">
+            {this.state.articles.map((news, index) => {
+              return (
+                <Newsitem
+                  key={index}
+                  imageUrl={news.urlToImage}
+                  title={`${news.title.slice(0, 55)}..`}
+                  description={
+                    news.description ? news.description.slice(0, 88) : ""
+                  }
+                  source={news.source.name}
+                  newsUrl={news.url}
+                  author={news.author}
+                  date={news.publishedAt}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
+        <div className="pagination">
+          <button
+            className="app__btns_primary"
+            disabled={this.state.page <= 1}
+            onClick={this.handlePrevBtn}
+          >
+            Previus
+          </button>
+          <button
+            className="app__btns_primary"
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            onClick={this.handleNextBtn}
+          >
+            Next
+          </button>
+        </div>
+      </>
     );
   }
 }
